eeea683943f6dcd1e262261b7d3484ffc94f13ac
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-07-15 22:33:49 -0300

    feat: setting dependency injection

---
+++ src/BarberBoss.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,29 @@
+﻿using BarberBoss.Application.AutoMapper;
+using BarberBoss.Application.UseCases.Invoices.GetAll;
+using BarberBoss.Application.UseCases.Invoices.GetById;
+using BarberBoss.Application.UseCases.Invoices.Register;
+using BarberBoss.Application.UseCases.Invoices.Update;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BarberBoss.Application;
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddUseCases(services);
+        AddMapper(services);
+    }
+
+    private static void AddUseCases(this IServiceCollection services) 
+    { 
+        services.AddScoped<IRegisterInvoiceUseCase, RegisterInvoiceUseCase>();
+        services.AddScoped<IGetAllInvoicesUseCase, GetAllInvoicesUseCase>();
+        services.AddScoped<IGetInvoiceByIdUseCase, GetInvoiceByIdUseCase>();
+        services.AddScoped<IInvoiceUpdateUseCase, InvoiceUpdateUseCase>();
+    }
+
+    private static void AddMapper(this IServiceCollection services) 
+    { 
+        services.AddAutoMapper(typeof(AutoMapping));
+    }
+}
---
+++ src/BarberBoss.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using BarberBoss.Domain.Repositories;
+using BarberBoss.Infrastructure.DataAccess;
+using BarberBoss.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BarberBoss.Infrastructure;
+public static class DependencyInjectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddRepositories(services);
+
+        AddDbContext(services, configuration);
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IWriteOnlyInvoiceRepository, InvoiceRepository>();
+        services.AddScoped<IReadOnlyInvoiceRepository, InvoiceRepository>();
+        services.AddScoped<IUpdateOnlyInvoiceRepository, InvoiceRepository>();
+    }
+
+    private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Connection");
+
+        var version = new Version(8, 0, 37);
+        var serverVersion = new MySqlServerVersion(version);
+
+        services.AddDbContext<BarberBossDbContext>(config => config.UseMySql(connectionString, serverVersion));
+    }
+}

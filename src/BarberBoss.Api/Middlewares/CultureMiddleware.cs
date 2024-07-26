cb18efcd6a844c77759b2786c2d3782bcb84eaba
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-07-25 23:38:43 -0300

    feat: created middleware for language

---
+++ src/BarberBoss.Api/Middlewares/CultureMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace BarberBoss.Api.Middlewares;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate neext)
+    {
+        _next = neext;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+        var requestedLanguage = context.Request.Headers.Accept.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("en");
+
+        if (!string.IsNullOrWhiteSpace(requestedLanguage)
+            && supportedLanguages.Exists(language => language.Name.Equals(requestedLanguage)))
+        {
+            cultureInfo = new CultureInfo(requestedLanguage);
+        }
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+    } 
+}

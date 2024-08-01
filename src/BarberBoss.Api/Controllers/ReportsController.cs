c2b781f4884b3340615c2f4bc61d12cd5efa875e
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-07-31 23:50:29 -0300

    fix: action return file type and parameter name

--- src/BarberBoss.Api/Controllers/ReportsController.cs
+++ src/BarberBoss.Api/Controllers/ReportsController.cs
@@ -11,9 +11,9 @@ public class ReportsController : ControllerBase
     [HttpGet("excel")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> GetExcel([FromServices] IGenerateInvoiceReportExcelUseCase useCase, [FromHeader] DateOnly month)
+    public async Task<IActionResult> GetExcel([FromServices] IGenerateInvoiceReportExcelUseCase useCase, [FromHeader] DateOnly week)
     {
-        byte[] file = await useCase.Execute(month);
+        byte[] file = await useCase.Execute(week);
 
         if (file.Length > 0)
             return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
@@ -24,12 +24,12 @@ public class ReportsController : ControllerBase
     [HttpGet("pdf")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> GetPdf([FromServices] IGenerateInvoiceReportPdfUseCase useCase, [FromHeader] DateOnly month)
+    public async Task<IActionResult> GetPdf([FromServices] IGenerateInvoiceReportPdfUseCase useCase, [FromHeader] DateOnly week)
     {
-        byte[] file = await useCase.Execute(month);
+        byte[] file = await useCase.Execute(week);
 
         if (file.Length > 0)
-            return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+            return File(file, MediaTypeNames.Application.Pdf, "report.pdf");
 
         return NoContent();
     }

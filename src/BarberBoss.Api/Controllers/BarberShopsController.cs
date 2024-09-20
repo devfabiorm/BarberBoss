f4e0b1ca9ce05f667613896786a02ac7dd34104e
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-20 01:24:20 -0300

    feat: Swagger documentation for barber shop register and list

--- src/BarberBoss.Api/Controllers/BarberShopsController.cs
+++ src/BarberBoss.Api/Controllers/BarberShopsController.cs
@@ -1,6 +1,7 @@
 ﻿using BarberBoss.Application.UseCases.BarberShops.GetAll;
 using BarberBoss.Application.UseCases.BarberShops.Register;
 using BarberBoss.Communication.Requests;
+using BarberBoss.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BarberBoss.Api.Controllers;
@@ -10,6 +11,8 @@ namespace BarberBoss.Api.Controllers;
 public class BarberShopsController : ControllerBase
 {
     [HttpPost]
+    [ProducesResponseType(typeof(ResponseBarberShopJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Register(
         [FromServices] IRegisterBarberShopUseCase useCase,
         [FromBody] RequestRegisterBarberShopJson request)
@@ -20,10 +23,17 @@ public class BarberShopsController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(typeof(ResponseBarberShopsJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public async Task<IActionResult> GetAll(
         [FromServices] IGetAllBarberShopsUseCase useCase)
     {
         var barberShops = await useCase.Execute();
+
+        if (barberShops.BarberShops.Count() == 0) 
+        {
+            return NoContent();
+        }
 
         return Ok(barberShops);
     }

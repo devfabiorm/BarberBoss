24b0a1ee8eebb2c997d73c3469a7c15570b240e5
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-22 19:27:06 -0300

    feat: finished token generation

--- src/BarberBoss.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ src/BarberBoss.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -5,6 +5,7 @@ using BarberBoss.Domain.Entities;
 using BarberBoss.Domain.Repositories;
 using BarberBoss.Domain.Repositories.Users;
 using BarberBoss.Domain.Security.Cryptography;
+using BarberBoss.Domain.Security.Tokens;
 using BarberBoss.Exception;
 
 namespace BarberBoss.Application.UseCases.Users.Register;
@@ -14,17 +15,20 @@ public class RegisterUserUseCase : IRegisterUserUseCase
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
     private readonly IPasswordEncrypter _passwordEncrypter;
+    private readonly IAccessTokenGenerator _tokenGenerator;
 
     public RegisterUserUseCase(
         IWriteOnlyUserRepository repository,
         IUnitOfWork unitOfWork,
         IMapper mapper,
-        IPasswordEncrypter passwordEncrypter)
+        IPasswordEncrypter passwordEncrypter,
+        IAccessTokenGenerator tokenGenerator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
         _mapper = mapper;
         _passwordEncrypter = passwordEncrypter;
+        _tokenGenerator = tokenGenerator;
     }
 
     public async Task<ResponseUserJson> Execute(RequestRegisterUserJson request)
@@ -39,7 +43,10 @@ public class RegisterUserUseCase : IRegisterUserUseCase
 
         await _unitOfWork.Commit();
 
-        return _mapper.Map<ResponseUserJson>(user);
+        var response =  _mapper.Map<ResponseUserJson>(user);
+        response.Token = _tokenGenerator.Generate(user);
+
+        return response;
     }
 
     private void Validate(RequestRegisterUserJson request)
--- src/BarberBoss.Infrastructure/DependencyInjectionExtension.cs
+++ src/BarberBoss.Infrastructure/DependencyInjectionExtension.cs
@@ -3,9 +3,11 @@ using BarberBoss.Domain.Repositories.BarberShops;
 using BarberBoss.Domain.Repositories.Invoices;
 using BarberBoss.Domain.Repositories.Users;
 using BarberBoss.Domain.Security.Cryptography;
+using BarberBoss.Domain.Security.Tokens;
 using BarberBoss.Infrastructure.DataAccess;
 using BarberBoss.Infrastructure.DataAccess.Repositories;
 using BarberBoss.Infrastructure.Security.Cryptography;
+using BarberBoss.Infrastructure.Security.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,7 +25,10 @@ public static class DependencyInjectionExtension
 
     private static void AddToken(IServiceCollection services, IConfiguration configuration)
     {
+        var expirationInMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpirationInMinutes");
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
 
+        services.AddScoped<IAccessTokenGenerator>(config => new JwtTokenGenerator(expirationInMinutes, signingKey!));
     }
 
     private static void AddRepositories(IServiceCollection services)

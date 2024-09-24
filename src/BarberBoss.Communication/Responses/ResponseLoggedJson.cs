0593b060936ee81d96774513ce48762024fe2698
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-23 23:16:15 -0300

    feat: Created classes for request and response on logging

---
+++ src/BarberBoss.Communication/Responses/ResponseLoggedJson.cs
@@ -0,0 +1,6 @@
+﻿namespace BarberBoss.Communication.Responses;
+public class ResponseLoggedJson
+{
+    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+}

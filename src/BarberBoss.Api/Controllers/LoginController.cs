849c3e0ad04b01fc92fa62f37cf1431d0688c535
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-23 23:15:02 -0300

    feat: Created methods to do login in the application

---
+++ src/BarberBoss.Api/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using BarberBoss.Application.UseCases.Login.DoLogin;
+using BarberBoss.Communication.Requests;
+using BarberBoss.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarberBoss.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseLoggedJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> DoLogin(
+            [FromServices] IDoLoginUseCase useCase,
+            [FromBody] RequestLoginJson request)
+        {
+            var response = await useCase.Execute(request);
+
+            return Ok(response);
+        }
+    }
+}

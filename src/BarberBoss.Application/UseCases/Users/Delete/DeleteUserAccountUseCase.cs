c4cfbf1c690439fcacbdf87f552c0ac08f491ce7
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-25 21:57:04 -0300

    feat: Created use case to handle user deletion

---
+++ src/BarberBoss.Application/UseCases/Users/Delete/DeleteUserAccountUseCase.cs
@@ -0,0 +1,29 @@
+﻿using BarberBoss.Domain.Repositories.Users;
+using BarberBoss.Domain.Services.LoggedUser;
+
+namespace BarberBoss.Application.UseCases.Users.Delete;
+public class DeleteUserAccountUseCase : IDeleteUserAccountUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IUpdateOnlyUserRepository _updateRepository;
+    private readonly IWriteOnlyUserRepository _writeRepository;
+
+    public DeleteUserAccountUseCase(
+        ILoggedUser loggedUser,
+        IUpdateOnlyUserRepository updateRepository,
+        IWriteOnlyUserRepository writeRepository)
+    {
+        _loggedUser = loggedUser;
+        _updateRepository = updateRepository;
+        _writeRepository = writeRepository;
+    }
+
+    public async Task Execute()
+    {
+        var loggedUser = await _loggedUser.Get();
+
+        var user = await _updateRepository.GetById(loggedUser.Id);
+
+        _writeRepository.Delete(user);
+    }
+}

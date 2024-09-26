1cd242455cacbbd8bafe5db767993ee5cffaaa85
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-25 21:56:37 -0300

    feat: Created use case to retrieve user data

---
+++ src/BarberBoss.Application/UseCases/Users/Profile/GetUserProfileUseCase.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BarberBoss.Communication.Responses;
+using BarberBoss.Domain.Services.LoggedUser;
+
+namespace BarberBoss.Application.UseCases.Users.Profile;
+public class GetUserProfileUseCase : IGetUserProfileUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IMapper _mapper;
+
+    public GetUserProfileUseCase(
+        ILoggedUser loggedUser,
+        IMapper mapper)
+    {
+        _loggedUser = loggedUser;
+        _mapper = mapper;
+    }
+
+    public async Task<ResponseUserJson> Execute()
+    {
+        var loggedUser = await _loggedUser.Get();
+
+        return _mapper.Map<ResponseUserJson>(loggedUser);
+    }
+}

0268d9c48d7336807292081766d7ddd737419371
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-10-01 23:58:55 -0300

    feat: created unit tests for user update

---
+++ tests/CommonTestUtilities/Requests/RequestUpdateUserJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using BarberBoss.Communication.Requests;
+using Bogus;
+
+namespace CommonTestUtilities.Requests;
+public class RequestUpdateUserJsonBuilder
+{
+    public static RequestUpdateUserJson Build()
+    {
+        return new Faker<RequestUpdateUserJson>()
+            .RuleFor(user => user.Name, faker => faker.Name.FirstName())
+            .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name));
+    }
+}
+
---
+++ tests/UseCases.Tests/Users/Update/UpdateUserUseCaseTests.cs
@@ -0,0 +1,100 @@
+﻿using BarberBoss.Application.UseCases.Users.Update;
+using BarberBoss.Domain.Entities;
+using BarberBoss.Exception;
+using BarberBoss.Exception.Messages;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases.Tests.Users.Update;
+public class UpdateUserUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var request = RequestUpdateUserJsonBuilder.Build();
+        var user = UserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        await act.Should().NotThrowAsync();
+    }
+
+    [Fact]
+    public async Task Name_Empty()
+    {
+        //Arrange
+        var request = RequestUpdateUserJsonBuilder.Build();
+        request.Name = string.Empty;
+        var user = UserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.NAME_REQUIRED));
+    }
+
+    [Fact]
+    public async Task Email_Empty()
+    {
+        //Arrange
+        var request = RequestUpdateUserJsonBuilder.Build();
+        request.Email = string.Empty;
+        var user = UserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.EMAIL_EMPTY));
+    }
+
+    [Fact]
+    public async Task Email_Invalid()
+    {
+        //Arrange
+        var request = RequestUpdateUserJsonBuilder.Build();
+        request.Email = "abc.ass";
+        var user = UserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.INVALID_EMAIL));
+    }
+
+    private UpdateUserUseCase CreateUseCase(User user)
+    {
+        var readOnlyRepository = new ReadOnlyUserRepositoryBuilder().GetByEmail(user.Email).Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var repository = UpdateOnlyUserRepositoryBuilder.Build();
+        var mapper = MapperBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build(); 
+
+        return new UpdateUserUseCase(readOnlyRepository, loggedUser, repository, mapper, unitOfWork);
+    }
+}

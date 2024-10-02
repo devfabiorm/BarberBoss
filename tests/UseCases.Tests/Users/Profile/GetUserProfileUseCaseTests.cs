08a98eb978fcce23a084de755bc1df53b74ad081
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-10-01 23:39:05 -0300

    feat: Created unit test for get user profile use case

---
+++ tests/UseCases.Tests/Users/Profile/GetUserProfileUseCaseTests.cs
@@ -0,0 +1,36 @@
+﻿using BarberBoss.Application.UseCases.Users.Profile;
+using BarberBoss.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using FluentAssertions;
+
+namespace UseCases.Tests.Users.Profile;
+public class GetUserProfileUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var user = UserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        //Act
+        var result = await useCase.Execute();
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(user.Id);
+        result.Name.Should().Be(user.Name);
+        result.Email.Should().Be(user.Email);
+    }
+
+    private GetUserProfileUseCase CreateUseCase(User user)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var mapper = MapperBuilder.Build();
+
+        return new GetUserProfileUseCase(loggedUser, mapper);
+    }
+}

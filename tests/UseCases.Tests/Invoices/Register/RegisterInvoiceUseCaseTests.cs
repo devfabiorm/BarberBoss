3f258549e8d584a80b6fa30a41aef6a9535a751c
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-10-03 22:15:02 -0300

    feat: Creates unit tests for invoice registration

--- tests/UseCases.Tests/Invoices/Register/RegisterInvoiceUseCaseTests.cs
+++ tests/UseCases.Tests/Invoices/Register/RegisterInvoiceUseCaseTests.cs
@@ -1,4 +1,6 @@
 ﻿using BarberBoss.Application.UseCases.Invoices.Register;
+using BarberBoss.Exception;
+using BarberBoss.Exception.Messages;
 using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
 using CommonTestUtilities.Requests;
@@ -24,6 +26,78 @@ public class RegisterInvoiceUseCaseTests
         result.Amount.Should().Be(request.Amount);
         result.Date.Should().Be(request.Date);
         result.PaymentType.Should().Be(request.PaymentType);
+    }
+
+    [Fact]
+    public async Task Error_Title_Empty()
+    {
+        //Arrange
+        var request = RequestInvoiceJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var useCase = CreateUseCase(request.BarberShopId);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.REQUIRED_TITLE));
+    }
+
+    [Fact]
+    public async Task Error_Future_Date()
+    {
+        //Arrange
+        var request = RequestInvoiceJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        var useCase = CreateUseCase(request.BarberShopId);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.INVOICE_DATE_NEEDS_OLDER_THAN_CURRENT));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-10)]
+    [InlineData(-55)]
+    public async Task Error_Amount_Zero_Or_Below(decimal amount)
+    {
+        //Arrange
+        var request = RequestInvoiceJsonBuilder.Build();
+        request.Amount = amount;
+
+        var useCase = CreateUseCase(request.BarberShopId);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.INVOICE_AMOUNT_GREATER_THAN_ZERO));
+    }
+
+    [Fact]
+    public async Task Error_Invalid_PaymentType()
+    {
+        //Arrange
+        var request = RequestInvoiceJsonBuilder.Build();
+        request.PaymentType = (BarberBoss.Communication.Enums.EPaymentType)100;
+
+        var useCase = CreateUseCase(request.BarberShopId);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.INVALID_PAYMENT_TYPE));
     }
 
     private RegisterInvoiceUseCase CreateUseCase(long? shopId = null)

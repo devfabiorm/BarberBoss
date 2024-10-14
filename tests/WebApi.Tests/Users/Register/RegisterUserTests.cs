f254593ca3f132be5b7f6abbca70a37f81551d50
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-10-13 23:57:05 -0300

    test: Created test for when trying register an user without name

--- tests/WebApi.Tests/Users/Register/RegisterUserTests.cs
+++ tests/WebApi.Tests/Users/Register/RegisterUserTests.cs
@@ -1,6 +1,9 @@
-﻿using CommonTestUtilities.Requests;
+﻿using BarberBoss.Exception.Messages;
+using CommonTestUtilities.Requests;
 using FluentAssertions;
+using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -31,5 +34,30 @@ public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
 
         responseBody.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
         responseBody.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace();
+    }
+
+    [Theory]
+    [InlineData("fr")]
+    [InlineData("pt-BR")]
+    [InlineData("pt-PT")]
+    [InlineData("en")]
+    public async Task Error_Empty_Name(string language)
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(language));
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var responseData = await result.Content.ReadAsStreamAsync();
+
+        var responseBody = await JsonDocument.ParseAsync(responseData);
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("NAME_REQUIRED", new CultureInfo(language));
+
+        var errors = responseBody.RootElement.GetProperty("errorMessages").EnumerateArray();
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
     }
 }

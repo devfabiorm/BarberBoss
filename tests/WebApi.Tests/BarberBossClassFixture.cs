1ba8a6c6eaabead3b49e773412c2972d5af51452
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-10-19 21:07:54 -0300

    feat: Created class to avoid code duplication

---
+++ tests/WebApi.Tests/BarberBossClassFixture.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace WebApi.Tests;
+public class BarberBossClassFixture : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    public BarberBossClassFixture(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+    }
+
+    protected async Task<HttpResponseMessage> DoGetAsync(string requestUri, string? token = null, string? language = null)
+    {
+        SetHeaders(token, language);
+
+        return await _httpClient.GetAsync(requestUri);
+    }
+
+    protected async Task<HttpResponseMessage> DoPostAsync(string requestUri, object requestBody, string? token = null, string? language = null)
+    {
+        SetHeaders(token, language);
+
+        return await _httpClient.PostAsJsonAsync(requestUri, requestBody);
+    }
+
+    protected async Task<HttpResponseMessage> DoPutAsync(string requestUri, object requestBody, string? token = null, string? language = null)
+    {
+        SetHeaders(token, language);
+
+        return await _httpClient.PutAsJsonAsync(requestUri, requestBody);
+    }
+
+    protected async Task<HttpResponseMessage> DoDeleteAsync(string requestUri, string? token = null, string? language = null)
+    {
+        SetHeaders(token, language);
+
+        return await _httpClient.DeleteAsync(requestUri);
+    }
+
+    private void SetHeaders(string? token = null, string? language = null)
+    {
+        if (token is not null)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        if (language is not null) 
+        { 
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(language));
+        }
+    }
+}
+ 

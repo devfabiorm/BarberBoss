9964c90fe55d44e5aaf3c3b0351fb233a89b0cb4
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-11-04 22:55:10 -0300

    feat: New migration

--- src/BarberBoss.Infrastructure/Migrations/20241105015122_InvoiceAndUserAndBarberShopRelationship.Designer.cs
+++ src/BarberBoss.Infrastructure/Migrations/20241105015122_InvoiceAndUserAndBarberShopRelationship.Designer.cs
@@ -4,6 +4,7 @@ using BarberBoss.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BarberBoss.Infrastructure.Migrations
 {
     [DbContext(typeof(BarberBossDbContext))]
-    partial class BarberBossDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241105015122_InvoiceAndUserAndBarberShopRelationship")]
+    partial class InvoiceAndUserAndBarberShopRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
